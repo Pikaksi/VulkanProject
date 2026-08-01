9001ad2b46a0234b1397ac7a45f3e3cd57617b7d
Pikaksi <miika.a@live.com>
2026-08-01 18:45:33 +0300

    windows compilation start

---
+++ shaders/winCompile.bat
@@ -0,0 +1,6 @@
+C:\VulkanSDK\1.4.357.0\Bin\glslc.exe shader3d.vert -o vert3d.spv
+C:\VulkanSDK\1.4.357.0\Bin\glslc.exe shader3d.frag -o frag3d.spv
+C:\VulkanSDK\1.4.357.0\Bin\glslc.exe shader2d.vert -o vert2d.spv
+C:\VulkanSDK\1.4.357.0\Bin\glslc.exe shader2d.frag -o frag2d.spv
+C:\VulkanSDK\1.4.357.0\Bin\glslc.exe sunShadows.vert -o vertSunShadows.spv
+C:\VulkanSDK\1.4.357.0\Bin\glslc.exe sunShadows.frag -o fragSunShadows.spv
